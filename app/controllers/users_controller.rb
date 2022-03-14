bf56ea2653e33cce7def24f1e0f51cd8377e0f8d
kmandawe <kdmandawe06@yahoo.com.ph>
2022-03-14 09:23:17 +0800

    Delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
   before_action :require_user, only: %i[edit update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Account and all associated articles successfully deleted'
+    redirect_to articles_path
+  end
+
   private
 
   def user_params
@@ -49,7 +56,7 @@ class UsersController < ApplicationController
 
   def require_same_user
     if current_user != @user
-      flash[:alert] = "You can only edit your own account"
+      flash[:alert] = 'You can only edit your own account'
       redirect_to @user
     end
   end
